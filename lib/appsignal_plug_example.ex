d78aa1a1ba644fffd69da7f2b392f542a82b4721
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2017-08-02 16:03:04 +0200

    Add instrumentation for the Plug app

--- lib/appsignal_plug_example.ex
+++ lib/appsignal_plug_example.ex
@@ -1,14 +1,18 @@
 defmodule AppsignalPlugExample do
   use Plug.Router
+  use Appsignal.Instrumentation.Decorators
 
   plug :match
   plug :dispatch
+
+  use Appsignal.Plug
 
   get "/" do
     slow()
     send_resp(conn, 200, "Welcome")
   end
 
+  @decorate transaction_event()
   defp slow do
     :timer.sleep(1000)
   end
