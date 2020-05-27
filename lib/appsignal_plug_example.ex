2a2f880dd24dde3c5bb8160aa84a9cce848825d7
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-05-27 11:17:43 +0200

    $ mix new appsignal_plug_example --sup

---
+++ lib/appsignal_plug_example.ex
@@ -0,0 +1,18 @@
+defmodule AppsignalPlugExample do
+  @moduledoc """
+  Documentation for AppsignalPlugExample.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AppsignalPlugExample.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/appsignal_plug_example_test.exs
@@ -0,0 +1,8 @@
+defmodule AppsignalPlugExampleTest do
+  use ExUnit.Case
+  doctest AppsignalPlugExample
+
+  test "greets the world" do
+    assert AppsignalPlugExample.hello() == :world
+  end
+end
